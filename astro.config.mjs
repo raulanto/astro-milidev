f4b26868481907b2680d59ab0fbaa4829cc8e3af
raulanto <74162376+raulanto@users.noreply.github.com>
2025-02-17 22:23:02 -0600

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,12 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
-import vue from '@astrojs/vue';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://bartoszlenar.github.io",
   base: "/astro-milidev/",
-  integrations: [tailwind(), sitemap(), mdx(), pagefind(),vue()],
+  integrations: [tailwind(), sitemap(), mdx(), pagefind()],
   markdown: {
     shikiConfig: {
       theme: "css-variables",
